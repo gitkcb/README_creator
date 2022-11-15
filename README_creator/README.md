8fd9369211e506cf432edc247ff78ffd93417675
gitkcb <kcharlesbell@gmail.com>
2022-11-15 17:18:58 -0500

    fixed bugs

--- README_creator/README.md
+++ README_creator/README.md
@@ -1,33 +1,34 @@
-# kevin
-[![GNUv3.0 license](https://img.shields.io/badge/License-GNUv3.0-blue.svg)]
+# 
+![Apache2.0 license](https://img.shields.io/badge/License-Apache2.0-blue.svg)
 
-                ##Table of Contents 
-                 [Description](#Description)
-                 [Usage](#Usage)
-                 [Credits](#Credits)
-                 [License](#License)
-                 [Tests](#Tests)
+## Table of Contents 
+- [Description](#description)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Tests](#tests)
 
-                ##Description
-                kevin
-                
-                ##Usage
-                kevin
-
-                ##Credits
-                kevin
-
-                ##License
-                GNUv3.0
-                undefined
-
-                ##Questions
-                My GitHub Repo: https://github.com/kevin
-                Any further Questions reach me at kevib
+## Description
 
 
-                ##Tests
-                kevib
+## Usage
+
+
+## Credits
+
+
+## License
+Apache2.0
+
+https://www.apache.org/licenses/LICENSE-2.0
+
+## Questions
+My GitHub Repo: https://github.com/
+Any further Questions reach me at 
+
+
+## Tests
+
 
 
 
--- README_creator/utils/generateMarkdown.js
+++ README_creator/utils/generateMarkdown.js
@@ -6,7 +6,7 @@ function renderLicenseBadge(license) {
   if(!license){
     return '';
  }else{
-  return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)]`
+  return `![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)`
  }
 }
 
@@ -14,29 +14,25 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === "MIT"){
-    return 
-    "https://lbesson.mit-license.org/"
+    console.log("i ran");
+    return "https://lbesson.mit-license.org/"
   }
   else if(license === "Apache2.0"){
-    return 
-    `https://www.apache.org/licenses/LICENSE-2.0`
+    return `https://www.apache.org/licenses/LICENSE-2.0`
   }
   else if(license === "GNUv3.0"){
-    return 
-    `https://www.gnu.org/licenses/gpl-3.0.en.html`
+    return  `https://www.gnu.org/licenses/gpl-3.0.en.html`
   }else if(license === "EPL"){
-    return 
-    `https://www.eclipse.org/legal/epl-2.0/`
+    return `https://www.eclipse.org/legal/epl-2.0/`
   }else if(license === "Unilicense"){
-    return 
-    `https://unlicense.org/`
+    return  `https://unlicense.org/`
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  `${renderLicenseLink(license)}`
+return`${renderLicenseLink(license)}`
 }
 
 // TODO: Create a function to generate markdown for README
@@ -44,33 +40,34 @@ function generateMarkdown(data) {
   return `# ${data.title}
 ${renderLicenseBadge(data.license)}
 
-                ##Table of Contents 
-                 [Description](#Description)
-                 [Usage](#Usage)
-                 [Credits](#Credits)
-                 [License](#License)
-                 [Tests](#Tests)
+## Table of Contents 
+- [Description](#description)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Tests](#tests)
 
-                ##Description
-                ${data.description}
-                
-                ##Usage
-                ${data.usage}
+## Description
+${data.description}
 
-                ##Credits
-                ${data.contributors}
+## Usage
+${data.usage}
 
-                ##License
-                ${data.license}
-                ${renderLicenseSection(data.license)}
+## Credits
+${data.contributors}
 
-                ##Questions
-                My GitHub Repo: https://github.com/${data.github}
-                Any further Questions reach me at ${data.email}
+## License
+${data.license}
+
+${renderLicenseSection(data.license)}
+
+## Questions
+My GitHub Repo: https://github.com/${data.github}
+Any further Questions reach me at ${data.email}
 
 
-                ##Tests
-                ${data.test}
+## Tests
+${data.test}
 
 
 
