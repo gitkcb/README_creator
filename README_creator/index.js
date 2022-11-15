dec9d3eaa185314142dbb7e5770031cd55923a11
gitkcb <kcharlesbell@gmail.com>
2022-11-15 10:10:27 -0500

    synched init function

--- README_creator/index.js
+++ README_creator/index.js
@@ -51,11 +51,19 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    inquirer.prompt(questions)
-}
+    fs.writeFile(fileName, data, (err) => err ? console.error(err): console.log("Information has been saved"))
+    }
+
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer.prompt(questions)
+    .then((answer) => {
+        let readmeContent = `
+        `
+        writeToFile();
+    })
+ }
 
 // Function call to initialize app
 init();
